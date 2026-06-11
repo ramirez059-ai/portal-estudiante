03eef16bd25ad277c92422113d6b06c7b5c25689
AdanR <adnram07@gmail.com>
2026-06-10 21:57:30 -0600

    cambios estilos

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 function mostrar(id, btn) {
-  document.querySelectorAll(".seccion").forEach(s => {
-    s.classList.remove("activa");
-  });
-
-  document.querySelectorAll(".tab-btn").forEach(b => {
-    b.classList.remove("activo");
-  });
-
+  document.querySelectorAll(".seccion").forEach(s => s.classList.remove("activa"));
+  document.querySelectorAll(".tab-btn").forEach(b => b.classList.remove("activo"));
   document.getElementById(id).classList.add("activa");
-
   if (btn) btn.classList.add("activo");
 }
 
 function irACalificaciones() {
   const tabCalif = document.querySelector(".tab-btn:nth-child(2)");
   mostrar("calificaciones", tabCalif);
+}
+
+function toggleDetalles(btn) {
+  const lista = document.getElementById("datos-lista");
+  lista.classList.toggle("visible");
+  btn.textContent = lista.classList.contains("visible") ? "Ocultar" : "Detalles";
 }
